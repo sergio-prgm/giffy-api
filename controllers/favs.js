4ce5af19f8865b5e23045025ecf460440f3a2759
sergio-dev <sergio-dev@outlook.com>
2022-05-16 13:20:07 +0200

    Feat: 'get favs' route

--- controllers/favs.js
+++ controllers/favs.js
@@ -3,9 +3,11 @@ const User = require('../models/User')
 const userExtractor = require('../middleware/userExtractor')
 
 // Retrieve favs from user
-favsRouter.get('/', async (request, response) => {
-  // find user and send the favs array
-  response.send('<h2>Esto son los favs</h2>')
+favsRouter.get('/', userExtractor, async (request, response) => {
+  const { userId } = request
+
+  const user = await User.findById({ _id: userId })
+  response.json(user.favorites)
 })
 
 // ✅ Add note to favorites
@@ -31,7 +33,7 @@ favsRouter.post('/', userExtractor, async (request, response) => {
   }
 })
 
-// Delete note from favs
+// ✅ Delete note from favs
 favsRouter.delete('/', userExtractor, async (request, response) => {
   const { id } = request.body
   const { userId } = request
